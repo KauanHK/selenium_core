415e11e7533d7882f3c5e3898282b149484da9c8
KauanHK <kauankaestner06@gmail.com>
2025-07-27 14:04:58 -0300

    feat: Wait.until() para outras condições e tratamento dos parâmetros timeout, poll_frequency e ignored_exceptions movido para o próprio wrapper em Wait

--- selenium_core/wait.py
+++ selenium_core/wait.py
@@ -2,8 +2,10 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.types import WaitExcTypes
-from typing import Callable
+from typing import Callable, TypeVar
 
+
+T = TypeVar('T')
 
 class Wait:
 
@@ -19,6 +21,44 @@ class Wait:
         self._default_timeout = default_timeout
         self._default_poll_frequency = default_poll_frequency
         self._default_ignored_exceptions = default_ignored_exceptions
+    
+    def until(
+        self,
+        condition: Callable[..., T],
+        timeout: float | None = None,
+        poll_frequency: float | None = None,
+        ignored_exceptions: WaitExcTypes | None = None
+    ) -> T:
+        """Aguarda até que a condição especificada seja atendida."""
+        timeout, poll_frequency, ignored_exceptions = self._get_wait_params(
+            timeout, poll_frequency, ignored_exceptions
+        )
+        return WebDriverWait(self._driver, timeout, poll_frequency, ignored_exceptions).until(condition)
+
+    def _get_timeout(self, timeout: float | None) -> float:
+        """Retorna o tempo limite padrão se nenhum for especificado."""
+        return timeout if timeout is not None else self._default_timeout
+    
+    def _get_poll_frequency(self, poll_frequency: float | None) -> float | None:
+        """Retorna a frequência de verificação padrão se nenhuma for especificada."""
+        return poll_frequency if poll_frequency is not None else self._default_poll_frequency
+    
+    def _get_ignored_exceptions(self, ignored_exceptions: WaitExcTypes | None) -> WaitExcTypes | None:
+        """Retorna as exceções ignoradas padrão se nenhuma for especificada."""
+        return ignored_exceptions if ignored_exceptions is not None else self._default_ignored_exceptions
+
+    def _get_wait_params(
+        self,
+        timeout: float | None,
+        poll_frequency: float | None,
+        ignored_exceptions: WaitExcTypes | None
+    ) -> tuple[float, float | None, WaitExcTypes | None]:
+        """Obtém os parâmetros de espera com valores padrão se não forem especificados."""
+        
+        timeout = self._get_timeout(timeout)
+        poll_frequency = self._get_poll_frequency(poll_frequency)
+        ignored_exceptions = self._get_ignored_exceptions(ignored_exceptions)
+        return timeout, poll_frequency, ignored_exceptions
 
     def __getattr__(self, name: str) -> Callable:
         
@@ -27,12 +67,19 @@ class Wait:
             raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
 
         def wait_wrapper(*args, **kwargs) -> any:
+            
+            timeout, poll_frequency, ignored_exceptions = self._get_wait_params(
+                kwargs.pop('timeout', None),
+                kwargs.pop('poll_frequency', None),
+                kwargs.pop('ignored_exceptions', None)
+            )
 
-            timeout = kwargs.pop('timeout', self._default_timeout)
-            poll_frequency = kwargs.pop('poll_frequency', self._default_poll_frequency)
-            ignored_exceptions = kwargs.pop('ignored_exceptions', self._default_ignored_exceptions)
-
-            ec_predicate = condition(*args, **kwargs)
-            return WebDriverWait(self._driver, timeout, poll_frequency, ignored_exceptions).until(ec_predicate)
+            # O nome do primeiro parâmetro varia de acordo com a condição ('locator', 'mark', 'element')
+            # Por isso, passamos como primeiro argumento
+            if 'locator' in kwargs:
+                locator = kwargs.pop('locator')
+            else:
+                locator, *args = args[:]
+            return WebDriverWait(self._driver, timeout, poll_frequency, ignored_exceptions).until(condition(locator, *args, **kwargs))
 
         return wait_wrapper
